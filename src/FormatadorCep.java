5cd64e4444e02cbc4db285b3f3f5012443647522
Gabriel S. Jesus <gabriel_live12@outlook.com>
2024-08-13 15:54:05 -0300

    Arquivos para compreensão e treinamento de fluxos de excções na linguagem Java.

---
+++ src/FormatadorCep.java
@@ -0,0 +1,17 @@
+public class FormatadorCep {
+    public static void main(String[] args) {
+        try {
+            String cepFormatado = formatarCep("11111111");
+            System.out.println(cepFormatado);
+        } catch (CepInvalidoExcpetion e) {
+            System.out.println("O cep não corresponde com as regras.");
+        }
+
+    }
+    static String formatarCep(String cep) throws CepInvalidoExcpetion {
+        if (cep.length() != 8)
+            throw new CepInvalidoExcpetion();
+
+            return "11.111-111";
+    }
+}
